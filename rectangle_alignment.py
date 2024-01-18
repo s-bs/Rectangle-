f8effa42da51e6951155cacff3a49efef3a1614f
hope <sumubam191@gmail.com>
2024-01-18 14:22:56 +0545

    restructure

--- rectangle_alignment.py
+++ rectangle_alignment.py
@@ -1,58 +1,60 @@
 import cv2
 import numpy as np
 
-# Read the image
-original_image = cv2.imread("fft.jpg", cv2.IMREAD_COLOR)
-image = original_image[10:-10, 10:-10]
-imgray = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)[..., 0]
-ret, thresh = cv2.threshold(imgray, 20, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-mask = 255 - thresh
-contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+def align_rectangle(image_path, area_threshold=100):
+ 
+    original_image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    image = original_image[10:-10, 10:-10]
+    imgray = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)[..., 0]
+    ret, thresh = cv2.threshold(imgray, 20, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    mask = 255 - thresh
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+    result_image = original_image.copy()
 
-# Create a copy of the original image to draw rectangles on
-result_image = original_image.copy()
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > area_threshold: 
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(result_image, [box], 0, (0, 255, 0), 2)
 
-# Draw rectangles for all detected contours
-for contour in contours:
-    area = cv2.contourArea(contour)
-    if area > 100:  # You can adjust the minimum area threshold as needed
-        rect = cv2.minAreaRect(contour)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        cv2.drawContours(result_image, [box], 0, (0, 255, 0), 2)
+            # Crop and rotate the rectangles
+            scale = 1  # cropping margin, 1 == no margin
+            W = rect[1][0]
+            H = rect[1][1]
+            Xs = [i[0] for i in box]
+            Ys = [i[1] for i in box]
+            x1 = min(Xs)
+            x2 = max(Xs)
+            y1 = min(Ys)
+            y2 = max(Ys)
+            angle = rect[2]
+            
+            rotated = False
+            if angle < -45:
+                angle += 90
+                rotated = True
+            elif angle >= 45:
+                angle -= 90
+                rotated = True
+            
+            center = (int((x1+x2)/2), int((y1+y2)/2))
+            size = (int(scale*(x2-x1)), int(scale*(y2-y1)))
+            M = cv2.getRotationMatrix2D((size[0]/2, size[1]/2), angle, 1.0)
+            cropped = cv2.getRectSubPix(image, size, center)
+            cropped = cv2.warpAffine(cropped, M, size)
+            croppedW = W if not rotated else H
+            croppedH = H if not rotated else W
+            result_image = cv2.getRectSubPix(
+                cropped, (int(croppedW*scale), int(croppedH*scale)), (size[0]/2, size[1]/2))
 
-        scale = 1  # cropping margin, 1 == no margin
-        W = rect[1][0]
-        H = rect[1][1]
-        Xs = [i[0] for i in box]
-        Ys = [i[1] for i in box]
-        x1 = min(Xs)
-        x2 = max(Xs)
-        y1 = min(Ys)
-        y2 = max(Ys)
-        angle = rect[2]
-        # print('Angle:',angle)
-        rotated = False
-        if angle < -45 :
-            angle += 90
-            rotated = True
-        elif angle >= 45:
-          angle -= 90
-          rotated = True
-        
-        center = (int((x1+x2)/2), int((y1+y2)/2))
-        size = (int(scale*(x2-x1)), int(scale*(y2-y1)))
-        M = cv2.getRotationMatrix2D((size[0]/2, size[1]/2), angle, 1.0)
-        cropped = cv2.getRectSubPix(image, size, center)
-        cropped = cv2.warpAffine(cropped, M, size)
-        croppedW = W if not rotated else H
-        croppedH = H if not rotated else W
-        result_image = cv2.getRectSubPix(
-            cropped, (int(croppedW*scale), int(croppedH*scale)), (size[0]/2, size[1]/2))
+            cv2.imshow("Straight Align", result_image)
+            cv2.waitKey(0)
 
-        # Show the result image with the rectangle
-        cv2.imshow("Straight Align", result_image)
-        cv2.waitKey(0)
-        
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+image_path = 'fft.jpg'
+
+align_rectangle(image_path, area_threshold=100)
--- rectangle_numbering.py
+++ rectangle_numbering.py
@@ -1,94 +1,36 @@
 import cv2 as cv
 
-image = cv.imread("fft.jpg")
-gray = cv.cvtColor(image,cv.COLOR_BGR2BGRA)
-edge = cv.Canny(gray,50,150)
-contours,_= cv.findContours(edge,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-# cv.drawContours(image, contours, -1, (0, 255, 0), 3)
-clipped_images = []
-child_contours = []
+def process_length_and_display_contours(image_path, num_contours_to_display=4):
+    image = cv.imread(image_path)
 
-#  clip the rectangle
-for i, contour in enumerate(contours):
-    x, y, w, h = cv.boundingRect(contour)
-    # Ensure the coordinates are within the image bounds
-    x = max(0, x)
-    y = max(0, y)
-    w = min(w, image.shape[1] - x)
-    h = min(h, image.shape[0] - y)
-    # Clip the rectangle
-    clipped_rectangle = image[y:y + h, x:x + w]
-    # Save each clipped rectangle as a separate image
-    seprate = cv.imwrite(f"clip/clipped_image_{i}.jpg", clipped_rectangle)
-    # Read the saved image back and append to the list
-    saved_image = cv.imread(f"clip/clipped_image_{i}.jpg")
-    clipped_images.append(saved_image)
-    
-tleng=[]
+    if image is None:
+        print(f"Error: Image not loaded. Check the file path: {image_path}")
+        return
 
-for i, clipp in enumerate(clipped_images):
-    gray_clip = cv.cvtColor(clipp, cv.COLOR_BGR2BGRA)
-    edge_clip = cv.Canny(gray_clip,50, 200)
-    # Find contours with hierarchy
-    contours2, hierarchy = cv.findContours(edge_clip, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    total_length = 0
-    # Draw only the parent
-    for j, hier in enumerate(hierarchy[0]):
-        if hier[3] != -1:
-            # Draw the parent contours
-            # cv.drawContours(gray_clip, contours2, j, (0, 0, 255), 3)
-            # Iterate through sub-child contours (children of the current child)
-            for k, sub_child_hier in enumerate(hierarchy[0]):
-                # Check if the contour is a child of the current parent
-                if sub_child_hier[3] == j:
-                    cv.drawContours(gray_clip, contours2, k, (0, 255, 0), 2)
-                    length = cv.arcLength(contours2[k], True)
-                    total_length += length
-                    # print(f"Length of sub-child contour {k} in parent Contour {i}: {length}")
-    # print(f"Total length of sub-child contours in image {j}: {total_length}")
-    tleng.append({'length':total_length,
-                  'parent':j
-                  })
-    # sort_length = sorted(tleng)
-    # print(f"LENGTH OF LINE IN PARENT IMAGE {j}:", tleng)
-# print('data:',tleng)
-sorted_data = sorted(tleng, key=lambda x: x['length'])
-# print("sorted_data:",sorted_data)
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2BGRA)
 
-for i, clipp in enumerate(clipped_images):
-    gray_clipp = cv.cvtColor(clipp, cv.COLOR_BGR2BGRA)
-    edge_clipp = cv.Canny(gray_clipp,50, 200)
-    # Find contours with hierarchy
-    contours3, hierarchy1 = cv.findContours(edge_clipp, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    total_length = 0
-    # Draw only the parent
-    for j, hier in enumerate(hierarchy1[0]):
-        if hier[3] != -1:
-            # Draw the parent contours
-            # cv.drawContours(gray_clip, contours2, j, (0, 0, 255), 3)
-            # Iterate through sub-child contours (children of the current child)
-            for k, sub_child_hier in enumerate(hierarchy1[0]):
-                # Check if the contour is a child of the current parent
-                if sub_child_hier[3] == j:
-                    # cv.drawContours(gray_clipp, contours3, k, (0, 255, 0), 2)
-                    length = cv.arcLength(contours3[k], True)
-                    total_length += length
-    
-    # print('Length',total_length)
-    # print('sort',sorted_data)
-    position = next((i for i, data in enumerate(sorted_data) if data['length'] == total_length), None)
-    cv.putText(gray_clipp,str(position + 1), (250,150), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv.LINE_AA)
-    # if position is not None:
-    #     print(f"The position of total_length {total_length} in the sorted data is {position + 1}.")
-    # else:
-    #     print(f"Total_length {total_length} not found in the sorted data.")
-    
-    # cv.drawContours(image, contours3, parent_index, (0, 255, 0), 2)
-    cv.namedWindow("Clipped image Numbering",cv.WINDOW_NORMAL)
-    cv.resizeWindow("Clipped image Numbering", 400, 300)
-    cv.imshow("Clipped image Numbering",gray_clipp)
+    edge = cv.Canny(gray_image, 50, 150)
+
+    contours, hierarchy = cv.findContours(edge, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+
+    contour_image = image.copy()
+
+    tleng = [(cv.arcLength(contour, True), i) for i, contour in enumerate(contours) if hierarchy[0][i][3] != -1]
+    tleng.sort()
+
+    line_numbers = {length_index[1]: i + 1 for i, length_index in enumerate(tleng)}
+
+    for len, index in tleng[:num_contours_to_display]:
+        contour = contours[index]
+        cv.drawContours(contour_image, [contour], -1, (0, 255, 0), 2)
+        number = line_numbers[index]
+        cv.putText(contour_image, str(number), tuple(contour[0][0]), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv.namedWindow("Contour Image", cv.WINDOW_NORMAL)
+    cv.imshow("Contour Image", contour_image)
     cv.waitKey(0)
     cv.destroyAllWindows()
-    
 
-    
+image_path = 'fft.png'
+
+process_length_and_display_contours(image_path, num_contours_to_display=4)
